566c10a66fcbf03ee18776bfef1b45d91d891274
vladimir454 <fantasy_fast78@hotmail.com>
2019-06-28 11:03:50 -0400

    Add files via upload
    index cmbios

---
+++ personalizados/offcanvas.js
@@ -0,0 +1,15 @@
+
+  function openNav()
+  {
+    document.getElementById("navbarsExampleDefault").style.width = "250px";
+    document.getElementById("main").style.margin = "0 -25px";
+    document.getElementById("navbar").style.marginRight = "250px";
+    // document.getElementById("navbar2").style.margin = "0250px";
+  }
+  function closeNav() {
+    document.getElementById("navbarsExampleDefault").style.width = "0";
+    document.getElementById("main").style.margin= "auto";
+    document.getElementById("navbar").style.margin = "0";
+    // document.getElementById("navbar2").style.margin = "0";
+
+  }
